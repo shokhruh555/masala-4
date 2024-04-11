383f65d588f66ea80720d569f4a75163acf184a8
Shokhruh555 <Shohruhismatov54@gmail.com>
2024-04-11 17:28:30 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,66 @@
+// MASALA 2
+
+// let x = 1;
+
+// let y = 4;
+
+// let z = 3;
+
+// if (x >= y && x >= z) {
+//     console.log(x);
+// }else if (y >= x && y >= z) {
+//     console.log(y);
+// }else {
+//     console.log(z);
+// }
+
+
+// MASALA 3
+
+// let oyRaqami = 12
+
+// let oyKuni;
+
+// switch (oyRaqami) {
+//     case 12:
+//     oyKuni = 31
+//         break;
+//     case 1:
+//     oyKuni = 31
+//         break;
+//     case 2:
+//     oyKuni = 31
+//         break;
+//     case 3:
+//     oyKuni = 28
+//         break;
+//     case 4:
+//     oyKuni = 31
+//         break;
+//     case 5:
+//     oyKuni = 30
+//         break;
+//     case 6:
+//     oyKuni = 31
+//         break;
+//     case 7:
+//     oyKuni = 30
+//         break;
+//     case 8:
+//     oyKuni = 31
+//         break;
+//     case 9:
+//     oyKuni = 30
+//         break;
+//     case 10:
+//     oyKuni = 31
+//         break;
+//     case 11:
+//     oyKuni = 30
+//         break;
+
+//     default: "bunday oy yo'q"
+//         break;
+// }
+
+// console.log(oyKuni);
